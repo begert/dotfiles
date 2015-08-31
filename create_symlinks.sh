b93879e103c31afa88b484b1e0b67fbe386eebe6
maethu <begi@gmx.ch>
2015-08-31 01:21:50 -0700

    add tmux and zshfunctions

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
 mv -v ~/.vimrc ~/.vimrc_old 2> /dev/null
 ln -s ~/dotfiles/vimrc ~/.vimrc
 
 mv -v ~/.vim ~/.vim_old 2> /dev/null
 ln -s ~/dotfiles/vim ~/.vim
+
+mv -v ~/.tmux.conf ~/.tmux.conf_old 2> /dev/null
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
