02ee0961271073961dff42de5fccbe230e19ac35
maethu <begi@gmx.ch>
2015-09-03 20:21:50 -0700

    rename install script and add vundle install to it

--- install.sh
+++ install.sh
@@ -6,5 +6,11 @@ ln -s ~/dotfiles/vimrc ~/.vimrc
 mv -v ~/.vim ~/.vim_old 2> /dev/null
 ln -s ~/dotfiles/vim ~/.vim
 
+mv -v ~/.nvimrc ~/.nvimrc_old 2> /dev/null
+ln -s ~/dotfiles/vimrc ~/.nvimrc
+
 mv -v ~/.tmux.conf ~/.tmux.conf_old 2> /dev/null
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/dotfiles/vim/bundle/Vundle.vim
+vim +PluginInstall +qall
